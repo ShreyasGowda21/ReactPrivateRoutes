018212e28c868e08be85998a73a6a7d3a719ff0f
ShreyasGowda21 <shreyushreyasgowda4@gmail.com>
2026-06-20 08:06:17 +0530

    api call

--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -1,9 +1,29 @@
-
+import { useEffect, useState } from "react"
 const Profilepage = () => {
+
+const[users,setUsers]=useState([])
+
+
+useEffect(()=>{
+fetch("https://api.github.com/users")
+.then((x)=>x.json())
+.then((data)=>setUsers(data))
+},[])
+console.log(users)
+
   return (
     <>
     <div>
       <center><h1>Welcome to Profile page</h1></center>
+      {
+        users.map((user)=>(
+          <div key={user.id}>
+             <img src={user.avatar_url} width='100px' height='100px'/>
+            <p>{user.login}</p>
+            </div>
+        ))
+      }
+
     </div>
     </>
   )
